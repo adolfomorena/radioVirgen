20ae931ca0dc68e20c1a819643b12274c0465fb2
adolfomorena <adomorang@gamil.com>
2025-01-31 00:01:59 +0100

    commit modelo creado

--- radiodb/admin.py
+++ radiodb/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Autor)
+admin.site.register(Programa)
+admin.site.register(Podcast)
+admin.site.register(Lista_Pendientes)
+admin.site.register(Lista_Escuchados)
+admin.site.register(MeGusta_Podcast)
+admin.site.register(MeGusta_Programa)
--- radiodb/models.py
+++ radiodb/models.py
@@ -1,3 +1,92 @@
 from django.db import models
+from django.db.models import CASCADE
+
 
 # Create your models here.
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+    nik = models.CharField(max_length=100, unique=True, null=True, blank=True)
+    correo = models.EmailField(unique=True)
+    fecha_nac = models.DateField()
+    fecha_registro = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellidos}"
+
+
+class Autor(models.Model):
+    nombre = models.CharField(max_length=100)
+    fecha_registro = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Programa(models.Model):
+    titulo = models.CharField(max_length=200)
+    fecha_publicacion = models.DateField()
+    fecha_baja= models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.titulo
+
+
+class Podcast(models.Model):
+    titulo = models.CharField(max_length=200)
+    fecha_publicacion = models.DateField()
+    fecha_baja = models.DateField(null=True, blank=True)
+    autores = models.ManyToManyField(Autor, related_name='autores')
+    categorias = models.CharField(max_length=100)
+    programa = models.ForeignKey(Programa, on_delete=models.CASCADE, null=True, blank=True, related_name='podcast_programa')
+    linkDrive = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.titulo
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(check=models.Q(categorias__in=["Educativo", "Comedia", "Formación"]), name='categorias_check')
+        ]
+
+class Lista_Pendientes(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=CASCADE, null=True, blank=True,related_name='lista_pendientes_usuario')
+    podcast = models.ForeignKey(Podcast, on_delete=CASCADE, null=True, blank=True,related_name='lista_pendientes_podcast')
+
+    def __str__(self):
+        return f"{self.usuario} {self.podcast}"
+
+    class Meta:
+        unique_together = ['usuario', 'podcast']
+
+class Lista_Escuchados(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=CASCADE, null=True, blank=True,related_name='lista_escuchados_usuario')
+    podcast = models.ForeignKey(Podcast, on_delete=CASCADE, null=True, blank=True,related_name='lista_escuchados_podcast')
+    fecha_escucha = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.usuario} {self.podcast}"
+
+    class Meta:
+        unique_together = ['usuario', 'podcast','fecha_escucha']
+
+class MeGusta_Podcast(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=CASCADE, null=True, blank=True,related_name='megusta_podcast_usuario')
+    podcast = models.ForeignKey(Podcast, on_delete=CASCADE, null=True, blank=True,related_name='megusta_podcast_podcast')
+
+    def __str__(self):
+        return f"{self.usuario} {self.podcast}"
+
+    class Meta:
+        unique_together = ['usuario', 'podcast']
+
+class MeGusta_Programa(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=CASCADE, null=True, blank=True,related_name='megusta_programa_usuario')
+    programa = models.ForeignKey(Programa, on_delete=CASCADE, null=True, blank=True,related_name='megusta_programa_programa')
+
+    def __str__(self):
+        return f"{self.usuario} {self.programa}"
+
+    class Meta:
+        unique_together = ['usuario', 'programa']
