361ca14aeb796dffb3f85f991fd123d0f8c99b36
adolfomorena <adomorang@gamil.com>
2025-02-18 17:29:51 +0100

    commit comando listar usuarios

---
+++ radiodb/management/commands/listarUsuarios.py
@@ -0,0 +1,21 @@
+from collections import UserList
+
+from django.core.management.base import BaseCommand
+from radiodb.models import *
+
+
+class Command(BaseCommand):
+    help = 'Listar usuarios'
+
+    def handle(self, *args, **kwargs):
+        usuarios = list(Usuario.objects.all())
+
+        try:
+            if not usuarios:
+                self.stdout.write(self.style.WARNING('No hay usuarios registrados en la base de datos.'))
+            else:
+                for user in usuarios:
+                    self.stdout.write(f'Nombre: {user.nombre} - Apellidos: {user.apellidos} - Nik: {user.nik} - ID: {user.id}')
+
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f'Error al listar los usuarios: {e}'))
